f8dc7f11ff2e16bde580acf6b9a638133491aafa
Super Writer <jatisari.innovation@gmail.com>
2016-03-24 11:50:06 +0700

    Initial commit

---
+++ app/src/main/java/net/agusharyanto/loginwithvolley/AppVar.java
@@ -0,0 +1,18 @@
+package net.agusharyanto.loginwithvolley;
+
+/**
+ * Created by agus on 3/23/16.
+ */
+public class AppVar {
+
+    //URL to our login.php file, url bisa diganti sesuai dengan alamat server kita
+    public static final String LOGIN_URL = "http://192.168.0.101/course/login.php";
+
+    //Keys for email and password as defined in our $_POST['key'] in login.php
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+
+    //If server response is equal to this that means login is successful
+    public static final String LOGIN_SUCCESS = "success";
+
+}
